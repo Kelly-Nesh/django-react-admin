2761a70de448ddcb2e3f4d623dcc3e44055d2786
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-27 16:12:33 +0300

    added cookies and queryclient defaults

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,47 @@
 import "./App.css";
-import Admin from "./admin/admin.jsx";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Login from "./admin/login.jsx";
+import Admin from "./admin/admin.jsx";
+import { CookiesProvider } from "react-cookie";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Layout /> },
+  { path: "/", element: <Outlet /> },
   {
     index: true,
     element: <Login />,
+  },
+
+  {
+    path: "admin",
+    element: <Admin />,
     children: [
       {
-        path: "admin",
-        element: <Admin />,
+        path: ":model/",
         children: [
-          {
-            path: ":model/",
-            children: [
-              { path: "add", element: <Outlet /> },
-              { path: ":item", element: <Outlet /> },
-            ],
-          },
+          { path: "add", element: <Outlet /> },
+          { path: ":item", element: <Outlet /> },
         ],
       },
     ],
   },
 ]);
 
-export const queryClient = new QueryClient({ retry: 5 });
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 5, refetchOnWindowFocus: false, refetchOnMount: false },
+  },
+});
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={true} />
+    <CookiesProvider defaultSetOptions={{ path: "/", sameSite: true }}>
+      <RouterProvider router={router} />
+    </CookiesProvider>
   </QueryClientProvider>
 );
 
