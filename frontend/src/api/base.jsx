9745a1577f16d572f1ddf78a36bec2c350ac7ac8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-28 05:05:10 +0300

    loading screen and base-url

--- frontend/src/api/base.jsx
+++ frontend/src/api/base.jsx
@@ -1,4 +1,16 @@
-const API_URL = "http://192.168.126.153:8000/api/admin/";
-export const HEADERS = { headers: { Authorization: "Token" } };
+import { MDBSpinner } from "mdb-react-ui-kit";
+
+export const BASE_URL = "http://192.168.126.153:8000";
+const API_URL = BASE_URL + "/api/admin/";
 export default API_URL;
-export const cl = console.log
+export const HEADERS = { headers: { Authorization: "Token" } };
+
+export const cl = console.log;
+
+export function LoadingScreen() {
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <MDBSpinner color="info" />
+    </div>
+  );
+}
