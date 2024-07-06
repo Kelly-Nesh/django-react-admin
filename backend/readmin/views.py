4b598d4a6f77597d2b8804f5c9ce62ed89b2d18e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-07-06 09:43:41 +0300

    homeview apiview list apps and models

--- backend/readmin/views.py
+++ backend/readmin/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import ml
 
-# Create your views here.
+
+class HomeView(APIView):
+    """API endpoint for getting list of registered models"""
+
+    def get(self, request):
+        """Return list of registered models
+            context (dict): 
+                AppName (str): Models (list)
+        """
+        context = {}
+        # Get list of models
+        for m in ml.models:
+            appName = m._meta.app_label.title()
+            if appName not in context:
+                context[appName] = []
+            context[appName].append(m.__name__)
+
+        return Response(context)
