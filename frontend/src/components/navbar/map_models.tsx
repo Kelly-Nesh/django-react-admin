e61c6f620c27954ba751b132018361fa57bde89e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-07-25 11:44:44 +0300

    has permission check for leftmenu apps and models

--- frontend/src/components/navbar/map_models.tsx
+++ frontend/src/components/navbar/map_models.tsx
@@ -1,5 +1,7 @@
+import "./navbar.css";
 import { Accordion } from "react-bootstrap";
 import { AppsModels } from "./navbar";
+import { cl } from "../../App";
 
 export const ModelMap = (data: AppsModels) => {
   // Checks permissions of models and sets auth app as the first in models list
@@ -11,23 +13,30 @@ export const ModelMap = (data: AppsModels) => {
     models = models.slice(0, auth_idx).concat(models.slice(auth_idx + 1));
     models.unshift("auth");
   }
+  const hasModelViewPermission = (app: string, model: string) => {
+    if (!data.perms) return false;
+    // cl(`${app}.view_${model}`);
+    return data.perms.find((r) => r === `${app}.view_${model.toLowerCase()}`);
+  };
 
-  const mapped_models = models.map((key: string) => {
-    return (
-      <Accordion.Item eventKey={key}>
-        <Accordion.Header>{key}</Accordion.Header>
-        <Accordion.Body>
-          {data.models &&
-            data.models[key].map((m: any) => <p className="m-0 mb-1">{m}</p>)}
-        </Accordion.Body>
-      </Accordion.Item>
-    );
+  const mapped_apps = models.map((key: string) => {
+    const p_models: Array<JSX.Element> = [];
+    data.models[key].forEach((m: string) => {
+      if (hasModelViewPermission(key, m)) {
+        p_models.push(<p className="m-0 mb-1" key={m}>{m}</p>);
+      }
+    });
+    // cl(p_models);
+    if (p_models.length > 0) {
+      return (
+        <Accordion.Item eventKey={key} key={key}>
+          <Accordion.Header>{key}</Accordion.Header>
+          <Accordion.Body>{p_models}</Accordion.Body>
+        </Accordion.Item>
+      );
+    }
   });
-  return <>{mapped_models}</>;
+  return <>{mapped_apps}</>;
 };
 
 export default ModelMap;
-
-// check if auth app is present
-// get the index and pop/remove
-// unshift it to first index
