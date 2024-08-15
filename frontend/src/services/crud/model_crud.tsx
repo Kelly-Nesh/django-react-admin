4d928223777c7392f28e2577035eca350eb2d0ce
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-08-15 08:07:59 +0300

    added id to share function for bulk retrieve and single retrieve

--- frontend/src/services/crud/model_crud.tsx
+++ frontend/src/services/crud/model_crud.tsx
@@ -2,18 +2,19 @@ import axios from "axios";
 import { API_URL, refreshToken } from "../auth/login";
 import { cl } from "../../App";
 
-export const modelGet = (token: string, app: string, model: string) => {
+export const modelGet = (
+  token: string,
+  app: string,
+  model: string,
+  id: number = -1
+) => {
   if (typeof token !== "string") return;
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  const MODEL_URL = `${API_URL}/${app}/${model}/`;
+  const MODEL_URL = `${API_URL}/${app}/${model}/${id !== -1 ? id + "/" : ""}`;
 
   return axios
     .get(MODEL_URL)
     .then((response) => {
-      cl(response.status);
-      if (response.status === 401) {
-        refreshToken();
-      }
       return response.data;
     })
     .catch((err) => {
